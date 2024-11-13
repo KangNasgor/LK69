60eee3d002805a3e7581df851510f5216572ba80
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-11-13 14:00:07 +0700

    making search feature shows all page instead of 1 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,18 +26,26 @@ export default function Home() {
   const [result, setResult] = useState("upcoming");
 
   const searchMovie = async () => {
-    const response = await fetch(`https://www.omdbapi.com/?s=${search}&apikey=56419db&`);
-    const data = await response.json();
-    if (data.Response === "True") {
-      setMovies(data.Search);
-      setResult("search");
+    let page = 1;
+    while(true){
+      const response = await fetch(`https://www.omdbapi.com/?s=${search}&page=${page}&apikey=56419db&`);
+      const data = await response.json();
+
+      if (data.Error === "Too many results.") {
+        setError("Too many results.");
+        break;
+      }
+      else if(data.Result === "Movie not found!") {
+        setError("Movie not found.");
+        break;
+      }
+      movies.push(...data.Search);
+      if (data.Search.length < 10){
+        break;
+      }
+      page++
     }
-    else if (data.Error === "Too many results.") {
-      setError("Too many results.");
-    }
-    else {
-      setError("Movie not found.");
-    }
+    setResult("search");
   }
 
   useEffect(() => {
@@ -67,47 +75,61 @@ export default function Home() {
   }, []);
   return (
     <div className="h-fit">
-      <div className="w-6/12 bg-yellow-300 flex flex-col justify-center items-center py-10 mx-auto">
-        <h1 className="text-black">Search movie</h1>
-        <Form action={searchMovie} className="flex flex-col gap-4 w-6/12">
-          <input value={search} onChange={(e) => setSearch(e.target.value)} className="text-black px-3 rounded-md" />
+      <div className="w-full bg-yellow-300 flex flex-col justify-center items-center py-10 h-24 mx-auto">
+        <Form action={searchMovie} className="flex gap-4 w-3/12">
+          <input value={search} onChange={(e) => setSearch(e.target.value)} className="text-black px-3 rounded-md" placeholder="Search for movies"/>
           <button className="w-6/12 bg-blue-500 p-4 rounded-md text-black mx-auto">Search</button>
         </Form>
       </div>
-      <div className="">
-        <h1 className="text-black font-medium text-xl">Upcoming movies :</h1>
-        <div className="flex gap-3 overflow-x-auto whitespace-nowrap px-5">
+      <div style={{
+        display: result === "search" ? "block" : "none"
+      }}>
+        <h1 className="text-black font-medium text-xl mb-5">Search Result :</h1>
+        <div className="flex gap-5 flex-col pb-36">
           {
+            error ? (<p className="text-black">{error}</p>)
+            : 
             movies && movies.length > 0 ? (
               movies.map((movie: Movie) => (
-                <div key={movie.imdbID} className=" min-w-56 mx-auto text-black flex flex-col gap-2 items-center">
-                  {movie.Poster ? (
-                    <Image src={movie.Poster} width={500} height={500} alt="poster" className="object-cover" />
-                  ) :
-                    (
-                      <p>No poster available.</p>
-                    )
-                  }
+                <div key={movie.imdbID} className="w-10/12 mx-auto">
+                  <div className="text-black flex gap-2 items-start">
+                    {
+                      movie.Poster === "N/A" ? <p>No poster available.</p> : <Image src={movie.Poster} width={120} height={120} alt="poster" className="object-cover" />
+                    }
+                    <div className="w-full">
+                      <h1 className="w-full truncate">{movie.Title}</h1>
+                      <p className="w-fit">Released in {movie.Year}</p>
+                    </div>
+                  </div>
+                  <div className="h-px w-9/12 bg-black mt-5 opacity-50"></div>
+                </div>
+              ))
+            ) : (<p className="text-black">{error}</p>)
+          }
+        </div>
+      </div>
+      <div style={{
+        display: result === "upcoming" ? "block" : "none"
+      }} className="pb-36">
+        <h1 className="text-black font-medium text-xl mb-5">Upcoming movies :</h1>
+        <div className="flex gap-3 overflow-x-auto whitespace-nowrap px-5">
+          { 
+            error ?
+            <p className="text-black">{error}</p>
+            :
+            latestMovie && latestMovie.length > 0 ?
+              latestMovie.map((movie: UpcomingMovies) => (
+                <div key={movie.id} className="min-w-56 flex flex-col">
+                  <div className="">
+                    {movie.poster ? (<Image src={movie.poster} width={500} height={500} alt="img" className="object-cover" />) : (<p>No poster available</p>)}
+                  </div>
                   <div className="w-full">
-                    <h1 className="w-full truncate">{movie.Title}</h1>
-                    <p className="w-fit">Released in {movie.Year}</p>
+                    <h1 className="text-black w-full truncate">{movie.title}</h1>
                   </div>
                 </div>
               ))
-            ) :
-              latestMovie && latestMovie.length > 0 ?
-                latestMovie.map((movie: UpcomingMovies) => (
-                  <div key={movie.id} className="min-w-56 flex flex-col">
-                    <div className="">
-                      {movie.poster ? (<Image src={movie.poster} width={500} height={500} alt="img" className="object-cover" />) : (<p>No poster available</p>)}
-                    </div>
-                    <div className="w-full">
-                      <h1 className="text-black w-full truncate">{movie.title}</h1>
-                    </div>
-                  </div>
-                ))
-                :
-                (<p className="text-black">{error}</p>)
+              :
+              <p className="text-black">{error}</p>
           }
         </div>
       </div>

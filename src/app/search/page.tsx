7a297741b9f94723e4926e08f4e43c0447b55c5c
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-11-14 12:57:00 +0700

    fixing bug where suer can't search another movies after searching from dashboard

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -15,12 +15,14 @@ export default function MovieSearch() {
   const [error, setError] = useState("");
   const [movies, setMovies] = useState<Movie[]>([]);
   const [search, setSearch] = useState("");
+
   const router = useRouter();
   const params = useSearchParams();
-  const query = params.get("query");
+  const query = params.get("query"); // fetch the keyword from the URL parameter
 
-  const searchMovie = () => {
+  const handleSearchMovie = () => {
     router.push(`/search?query=${encodeURIComponent(search)}`); 
+    console.log(query);
   }
 
   useEffect( () => {
@@ -44,6 +46,11 @@ export default function MovieSearch() {
           }
           fetchedMovies.push(...data.Search); // Inserting all movies data
           if (data.Search.length < 10) { // If movies data is below 10, then stop the search
+            console.log("last page");
+            break;
+          }
+          if(fetchedMovies.length === 100){
+            console.log(data.Search.length)
             break;
           }
           page++
@@ -56,22 +63,22 @@ export default function MovieSearch() {
       setMovies(fetchedMovies);
     }
     searchMovie();
-  }, [search]);
+  }, [query]);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1); // tracks which page is user on
+  const itemsPerPage = 10; // set how many items should be displayed in 1 page
 
-  const totalPages = Math.ceil(movies.length / itemsPerPage); // math ceil to round the number
+  const totalPages = Math.ceil(movies.length / itemsPerPage); // math ceil is to round the number of total pages
 
   // Get current movies based on the page
   const currentMovies = movies.slice(
-    (currentPage - 1) * itemsPerPage,
+    (currentPage - 1) * itemsPerPage, // tracks which items should be displayed first, example : if the currentPage is 2 then subtracted by 1 and then multiplied by itemsPerPage which is 10 the first item would be index 10
     currentPage * itemsPerPage
   );
 
   // Handle page change
   const handlePageChange = (page: number) => {
-    if (page >= 1 && page <= totalPages) {
+    if (page >= 1 && page <= totalPages) { // if it is more/equal to 1 and is below/equal with totalPages
       setCurrentPage(page);
     }
   };
@@ -81,7 +88,7 @@ export default function MovieSearch() {
         <div className="w-full bg-yellow-300 flex flex-col justify-center items-center py-10 mb-10 h-24 mx-auto">
           <div className="flex gap-4 w-3/12">
             <input value={search} onChange={(e) => setSearch(e.target.value)} className="text-black px-3 rounded-md" placeholder="Search for movies"/>
-            <button className="w-6/12 bg-blue-500 p-4 rounded-md text-black mx-auto" onClick={searchMovie}>Search</button>
+            <button className="w-6/12 bg-blue-500 p-4 rounded-md text-black mx-auto" onClick={handleSearchMovie}>Search</button>
           </div>
         </div>
       {
